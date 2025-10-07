ab6b659dd5211b07b2970a059da1d81567e1a6a9
Your Name <you@example.com>
2025-10-07 10:53:30 +0200

    Ajout url mongo + ajout de livre

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,14 @@
 const express = require('express'); 
+const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+
+const Book = require ('./models/Book');
+
+mongoose.connect('mongodb+srv://Tim:0312@cluster0.dkst1xb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -7,6 +17,24 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app. use(bodyParser.json());
+
+app.post ('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book ({
+    ...req.body
+  });
+  book.save()
+    .then (()=> res.status (201).json({ message: 'Book enregistré'}))
+    .catch (error => res.status (400).json ({error}));
+});
+
+app.get ('/api/books', (req, res, next) => {
+  Book.find()
+  .then (books => res.status (200).json(books))
+  .catch (error => res.status (400).json ({error}));
 });
 
 module.exports = app;
