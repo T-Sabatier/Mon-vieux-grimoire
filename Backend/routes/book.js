b1e8b440b54d8f0efa846c613b62ea6732981051
T-Sabatier <t.sabatier04@laposte.net>
2025-10-14 12:12:24 +0200

    Ajout route rating

--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -5,10 +5,16 @@ const bookCtrl = require ('../controllers/book');
 const auth = require ('../middleware/auth');
 const multer = require ('../middleware/multer-congif');
 
-router.post ('/', auth, multer,bookCtrl.createBook);
-router.put ('/:id', auth,multer,stuffCtrl.modifyBook);
-router.delete ('/:id', auth,stuffCtrl.deleteBook);
+router.get ('/bestrating', bookCtrl.getBestRatedBooks);
+
+//Routes publiques
 router.get ('/:id', auth, bookCtrl,getOneBook);
-router.get ('/', auth,stuffCtrl.getAllBooks);
+router.get ('/', auth,bookCtrl.getAllBooks);
+
+//Routes protégées
+router.post ('/', auth, multer,bookCtrl.createBook);
+router.put ('/:id', auth,multer,bookCtrl.modifyBook);
+router.delete ('/:id', auth,bookCtrl.deleteBook);
+router.post ('/:id/rating', auth,bookCtrl.rateBook);
 
 module.exports = router;
