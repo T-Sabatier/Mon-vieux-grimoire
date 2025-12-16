4170d3725cd1b4afc5b4638fa694f840ca954ade
T-Sabatier <t.sabatier04@laposte.net>
2025-12-16 19:03:28 +0100

    Update backend URL to new service

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://mon-vieux-grimoire-o9v7.onrender.com/';
+const API_URL = 'https://mon-vieux-grimoire-api-g1t2.onrender.com';
 export const API_ROUTES = {
   SIGN_UP: `${API_URL}/api/auth/signup`,
   SIGN_IN: `${API_URL}/api/auth/login`,
